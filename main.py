d351789a070aeda3c6503090f80969b04c69d737
mikekravets <michkravets@gmail.com>
2022-11-16 11:17:32 +0200

    * Cover RonSwanson api with the tests * Remove main.py file

--- main.py
+++
@@ -1,7 +0,0 @@
-from pprint import pprint
-
-from ron_swanson.api import RonSwanson
-
-if __name__ == '__main__':
-    ron = RonSwanson()
-    pprint(ron.search("food"))
--- ron_swanson/api.py
+++ ron_swanson/api.py
@@ -1,3 +1,5 @@
+import typing
+
 import requests
 
 
@@ -6,12 +8,18 @@ class RonSwanson:
     def __init__(self, base_url: str = "https://ron-swanson-quotes.herokuapp.com"):
         self.base_url = base_url
 
-    def random(self, count: int = 1) -> list[str]:  # typing.List[str]
-        url = f"{self.base_url}/v2/quotes/{count}"
-        resp = requests.get(url, headers={'Content-Type': 'application/json'})
-        return resp.json()
+    def one(self) -> str:
+        url = f"{self.base_url}/v2/quotes/"
+        return self._get(url)
 
-    def search(self, term: str) -> list[str]:
+    def random(self, count: int = 1) -> typing.List[str]:
+        url = f"{self.base_url}/v2/quotes/{count}"
+        return self._get(url)
+
+    def search(self, term: str) -> typing.List[str]:
         url = f"{self.base_url}/v2/quotes/search/{term}"
+        return self._get(url)
+
+    def _get(self, url: str) -> typing.Union[typing.List[str], str]:
         resp = requests.get(url, headers={'Content-Type': 'application/json'})
         return resp.json()
